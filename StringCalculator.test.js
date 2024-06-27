b1241eaec59db5513d166ab238e6983f2c5dfe1f
Zubair Ansari <juberansari22@gmail.com>
2024-06-27 22:40:25 +0530

    Handle multiple numbers case

--- StringCalculator.test.js
+++ StringCalculator.test.js
@@ -1,5 +1,5 @@
 const StringCalculator = require("./StringCalculator");
-test("should return the sum of two numbers", () => {
+test("should return the sum of multiple numbers", () => {
   const calculator = new StringCalculator();
-  expect(calculator.add("1,2")).toBe(3);
+  expect(calculator.add("1,2,3")).toBe(6);
 });
